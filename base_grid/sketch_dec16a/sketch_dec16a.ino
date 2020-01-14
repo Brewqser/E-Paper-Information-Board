7456bdf3f08d78e4c6d65119d44941940305eb28
Brewqser <michal.czwornog124812@gmail.com>
2020-01-14 22:50:03 +0100

    Grid update

--- base_grid/sketch_dec16a/sketch_dec16a.ino
+++ base_grid/sketch_dec16a/sketch_dec16a.ino
@@ -3,9 +3,9 @@
 #include <GxGDEW075Z09/GxGDEW075Z09.h>    // 7.5" b/w/r
 
 //#include <Fonts/FreeMonoBold9pt7b.h>
-//#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
 #include <Fonts/FreeMonoBold18pt7b.h>
-//#include <Fonts/FreeMonoBold24pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
 
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
@@ -13,14 +13,17 @@
 GxIO_Class io(SPI, /*CS=5*/ 10, /*DC=*/ 9, /*RST=*/ 8);
 GxEPD_Class display(io, /*RST=*/ 8, /*BUSY=*/ 7);
 
-int numberOfRows = 3;
-int heightOffset = 75;
-int widhtOffset = 300;
+int numberOfHours = 3;
+int numberOfNews = 7;
+int heightOffset = 150;
+int widhtOffset = 40;
 int widht2 = 300;
 int spacing = 85;
+int NCFcounter = 100;
 
 String normalTimes[6] = {"12:00", "15:00", "11:00", "19:00", "9:00", "14:53"};
 String presentTimes[6] = {"12:00", "15:00", "11:00", "16:29", "12:23", "14:52"};
+String news[7] = {"Papier jest glupi", "NWM co napisac", "aaaaaaaa", "jest 24", "i jeszcze", "nie skonczylem", "xd"};
 
 void setup()
 {
@@ -35,25 +38,29 @@ void setup()
 
 void loop() {
   updateDisplay();
-  
+  Serial.println("asd");
   delay(1000);
 }
 
 void updateDisplay()
 {
   display.fillScreen(GxEPD_WHITE);
+
+  display.setRotation(3);
+
+  // godziny
  
-  display.fillRect(300, 75, 300, 3, GxEPD_BLACK);
-  for (int i=1;i<=numberOfRows;i++)
+  display.fillRect(widhtOffset, heightOffset, widht2, 3, GxEPD_BLACK);
+  for (int i=1;i<=numberOfHours;i++)
   {
-      display.fillRect(widhtOffset, 75 + i * 85 , widht2, 3, GxEPD_BLACK);
+      display.fillRect(widhtOffset, heightOffset + i * 85 , widht2 + 3, 3, GxEPD_BLACK);
   }
-  display.fillRect(widhtOffset, heightOffset, 3, numberOfRows * 85, GxEPD_BLACK);
-  display.fillRect(widhtOffset + widht2 / 2, heightOffset, 3, numberOfRows * 85, GxEPD_BLACK);
-  display.fillRect(widhtOffset + widht2, heightOffset, 3, numberOfRows * 85, GxEPD_BLACK);
+  display.fillRect(widhtOffset, heightOffset, 3, numberOfHours * 85, GxEPD_BLACK);
+  display.fillRect(widhtOffset + widht2 / 2, heightOffset, 3, numberOfHours * 85, GxEPD_BLACK);
+  display.fillRect(widhtOffset + widht2, heightOffset, 3, numberOfHours * 85, GxEPD_BLACK);
 
   display.setFont(&FreeMonoBold18pt7b);
-  for (int i=0;i<numberOfRows;i++)
+  for (int i=0;i<numberOfHours;i++)
   {
       display.setCursor(widhtOffset + 15, heightOffset + 15 + (i + 0.5) * spacing);
       if (normalTimes[2*i] == presentTimes[2*i]) display.setTextColor(GxEPD_BLACK);
@@ -65,6 +72,34 @@ void updateDisplay()
       else display.setTextColor(GxEPD_RED);
       display.println(presentTimes[2*i+1]);
   }
+
+  // news
+  
+  display.fillRect(0, 415, 384, 30, GxEPD_BLACK);
+  display.setCursor(150, 440);
+  display.setFont(&FreeMonoBold18pt7b);
+  display.setTextColor(GxEPD_WHITE);
+  display.println("NEWS"); 
+
+  display.setFont(&FreeMonoBold12pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  for (int i=0;i<numberOfNews;i++)
+  {
+      display.setCursor(5, 470 + (i * 24));
+      display.println(news[i]);
+  }
+
+
+  // NFC counter
+
+  display.fillRect(0, 120, 384, 10, GxEPD_BLACK);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold24pt7b);
+  display.setCursor(100, 70);
+  display.println("NCF: ");
+  display.setCursor(230, 70);
+  display.println(NCFcounter);
+  
   
   display.update();
 }
---
+++ https request/ConnectWithWPA/ConnectWithWPA.ino
@@ -0,0 +1,170 @@
+/*
+ * ConnectWithWPA
+ *
+ * This example is used to connect to an WPA2-encrypted WiFi network.
+ * Then it prints the MAC address of the WiFi module,
+ * the IP address obtained through DHCP, and other network information.
+ *
+ */
+
+#include <SPI.h>
+#include <WiFiST.h>
+
+/*
+  The following configuration is dedicated to the DISCO L475VG IoT board.
+  You should adapt it to your board.
+
+Configure SPI3:
+ * MOSI: PC12
+ * MISO: PC11
+ * SCLK: PC10
+
+Configure WiFi:
+ * SPI         : SPI3
+ * Cs          : PE0
+ * Data_Ready  : PE1
+ * reset       : PE8
+ * wakeup      : PB13
+ */
+
+SPIClass SPI_3(PC12, PC11, PC10);
+WiFiClass WiFi(&SPI_3, PE0, PE1, PE8, PB13);
+
+char ssid[] = "Brewqser";         // your network SSID (name)
+char pass[] = "244466666";  // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+  // initialize serial communication and wait for serial monitor to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  // initialize the WiFi module:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi module not detected");
+    // don't continue:
+    while (true);
+  }
+
+  // print firmware version:
+  String fv = WiFi.firmwareVersion();
+  Serial.print("Firmware version: ");
+  Serial.println(fv);
+
+  if (fv != "C3.5.2.5.STM") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA/WPA2 network: ");
+    Serial.println(ssid);
+    // Connect to WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    if (status != WL_CONNECTED) {
+      // Connect to WPA (TKIP) network:
+      status = WiFi.begin(ssid, pass, ES_WIFI_SEC_WPA);
+    }
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the connection info:
+  Serial.println("Connected.\nNetwork information:");
+  printCurrentNet();
+  printWifiData();
+
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  Serial.println("*** Network information ***");
+  printCurrentNet();
+}
+
+void printWifiData() {
+  // print your WiFi device's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the MAC address of your WiFi module:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  for (uint8_t i = 0; i < 6; i++) {
+    if (mac[i] < 0x10) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i != 5) {
+      Serial.print(":");
+    } else {
+      Serial.println();
+    }
+  }
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're connected to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're connected to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  for (uint8_t i = 0; i < 6; i++) {
+    if (bssid[i] < 0x10) {
+      Serial.print("0");
+    }
+    Serial.print(bssid[i], HEX);
+    if (i != 5) {
+      Serial.print(":");
+    } else {
+      Serial.println();
+    }
+  }
+
+  // print the received signal strength:
+  int32_t rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  Serial.print("Encryption Type: ");
+  printEncryptionType(WiFi.encryptionType());
+  Serial.println();
+}
+
+void printEncryptionType(uint8_t encryptionType) {
+  // read the encryption type and print out the name:
+  switch (encryptionType) {
+    case ES_WIFI_SEC_OPEN:
+      Serial.println("OPEN");
+      break;
+    case ES_WIFI_SEC_WEP:
+      Serial.println("WEP");
+      break;
+    case ES_WIFI_SEC_WPA:
+      Serial.println("WPA");
+      break;
+    case ES_WIFI_SEC_WPA2:
+      Serial.println("WPA2");
+      break;
+    case ES_WIFI_SEC_WPA_WPA2:
+      Serial.println("WPA_WPA2");
+      break;
+    case ES_WIFI_SEC_WPA2_TKIP:
+      Serial.println("WPA_TKIP");
+      break;
+    case ES_WIFI_SEC_UNKNOWN:
+    default:
+      Serial.println("UNKNOWN");
+      break;
+  }
+}
